d1e87d6fd892b8811ae0955263520aa616d35587
Conchy <xConchyx@gmail.com>
2024-06-14 14:48:05 +0200

    consumir API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { userHeroesStore } from '@/stores/superheroes';
+
+const heroesStore = userHeroesStore()
+
+heroesStore.get()
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div v-if="heroesStore.isLoading">
+
+      <div>{{ heroesStore.superHeroes[0].name }}</div>
+
+
+      <ul>
+        <li v-for="heroe in heroesStore.superHeroes">
+          <img :src="heroe.images.xs" alt="">
+          {{ heroe.name }};
+          {{ heroe.id }}
+          {{ heroe.powerstats }}
+          {{ heroe.appearance }}
+        </li>
+        <li> </li>
+      </ul>
+
+
+    </div>
+    <div v-else> Loading </div>
   </main>
 </template>
