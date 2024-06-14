17aeb8d0a7445f03583335608c66523b0accd94e
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-06-14 12:59:22 +0200

    footer done

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+      <footer class="footer">
+          <span >Supercoders F5 &copy; </span>
+        </footer>
+    </div>
+  </template>
+  
+  <style scoped lang="scss">
+ 
+
+  .footer {
+    width: 100%;
+    background-color: #F07E7E;
+    color: white;   
+    padding: 15px 0;
+    position: fixed;
+    bottom: 0;
+     
+  }
+  .footer span {
+    background-color: #F07E7E;
+    font-size: 16px;
+    font-weight: bold;
+    padding-left: 10px; 
+    display: inline-block;
+  }
+  </style>
+  
