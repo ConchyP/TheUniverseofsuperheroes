45bd7930a30fe9dcb9814cf185b3a13da410991c
NachoR9 <nachorm9@hotmail.com>
2024-06-19 15:26:35 +0200

    properties of cards added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,16 +7,53 @@ const props = defineProps({
 </script>
 
 <template>
+  <div class="">
   <div class="card" style="width: 18rem">
     <img :src="heroe.images.lg" class="card-img-top" alt="..." />
     <div class="card-body">
       <h5 class="card-title">{{ heroe.name }}</h5>
-      <p class="card-text">
-        {{ heroe.appearance.gender }}
+      <p class="card-text">Intelligence:
+        {{ heroe.powerstats.intelligence }}
       </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <p class="card-text">Strength:
+        {{ heroe.powerstats.strength }}
+      </p>
+      <p class="card-text">Speed:
+        {{ heroe.powerstats.speed }}
+      </p>
+      <p class="card-text">Power:
+        {{ heroe.powerstats.power }}
+      </p>
+      <p class="card-text">Durability:
+        {{ heroe.powerstats.durability }}
+      </p>
+      <p class="card-text">Combat:
+        {{ heroe.powerstats.combat }}
+      </p>
+
+      <a href="#" class="btn btn-primary">Favorites</a>
     </div>
+  </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+
+// .container {
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   gap: 1rem;
+
+
+//     .card {
+//   width: 100%;
+ 
+// } 
+// }
+  
+  
+  
+
+
+</style>
